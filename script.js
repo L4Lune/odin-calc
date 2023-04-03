418844f4c0c4983d1383c06fd5bf3efa8da7f30e
Jack Roudebush <roudebushjack@gmail.com>
2023-04-02 21:07:15 -0400

    Create main operator functions

---
+++ script.js
@@ -0,0 +1,20 @@
+function add(numOne, numTwo) {
+    result = numOne + numTwo;
+    return result;
+}
+
+function subtract(numOne, numTwo) {
+    result = numOne - numTwo;
+    return result;
+}
+
+function multiply(numOne, numTwo) {
+    result = numOne * numTwo;
+    return result;
+}
+
+function divide(numOne, numTwo) {
+    result = numOne / numTwo;
+    return result;
+}
+
